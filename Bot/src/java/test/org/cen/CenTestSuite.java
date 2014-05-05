91538150a81ca1259bcdfa7880984a550d3db533
Stephane Vanacker <svanacker@talentsoft.com>
2014-05-05 21:42:49 +0200

    Initial decomposition into tests - projects

--- Bot/src/java/test/org/cen/CenTestSuite.java
+++ Bot/src/java/test/org/cen/CenTestSuite.java
@@ -1,12 +1,8 @@
 package org.cen;
 
-import org.cen.com.ComDataUtilsTest;
 import org.cen.com.ComManagerTest;
-import org.cen.com.DefaultDecoderTest;
-import org.cen.math.MathUtilsTest;
 import org.cen.robot.control.RobotControlEngineTest;
 import org.cen.robot.device.RobotDeviceFactoryTest;
-import org.cen.robot.device.lcd.LcdDeviceTest;
 import org.cen.robot.device.navigation.analysis.com.MotionAnalysisDecoderTest;
 import org.cen.robot.device.navigation.com.NavigationDataDecoderTest;
 import org.junit.runner.RunWith;
@@ -14,8 +10,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses(value = { ComDataUtilsTest.class, ComManagerTest.class, MathUtilsTest.class, LcdDeviceTest.class,
-		NavigationDataDecoderTest.class, RobotDeviceFactoryTest.class, DefaultDecoderTest.class,
+@SuiteClasses(value = { ComManagerTest.class,  
+		NavigationDataDecoderTest.class, RobotDeviceFactoryTest.class, 
 		RobotControlEngineTest.class, MotionAnalysisDecoderTest.class })
 public class CenTestSuite {
 
